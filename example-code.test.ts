b462e8dd2f186138fdc1d059ca3355bf12a3c065
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2020-09-15 19:40:01 +0200

    yet another function with test

--- example-code.test.ts
+++ example-code.test.ts
@@ -1,4 +1,4 @@
-import { helloWorld } from './example-code';
+import { helloWorld, add } from './example-code';
 
 describe('example-code functions', () => {
 
@@ -13,5 +13,20 @@ describe('example-code functions', () => {
 
     // assert what is expected from function call
     expect(console.log).toHaveBeenCalledTimes(1);
+  })
+
+  it('add(): 1 + 2 = 3', () => {
+    const result = add(1,2);
+    expect(result).toEqual(3);
+  })
+
+  it('add(): undefined + 2 = NaN', () => {
+    const result = add(undefined,2);
+    expect(result).toEqual(NaN);
+  })
+
+  it('add(): undefined + undefined = NaN', () => {
+    const result = add(undefined,undefined);
+    expect(result).toEqual(NaN);
   })
 })
