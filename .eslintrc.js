22fa7746858055f0cafecbd46c392d8065fd9ac4
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2020-09-16 14:37:42 +0200

    #5 deleted some options in .eslintrc.js to fix the linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   parserOptions: {
     ecmaVersion: 9,
-    project: './tsconfig.json',
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
@@ -10,8 +9,4 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['plugin:@typescript-eslint/recommended','prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-  rules: {
-    '@typescipt-eslint/interface-name-prefix': ['always'],
-    'no-underscore-dangle': 'error',
-  },
 };
