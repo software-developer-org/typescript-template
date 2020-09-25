e77b9c758e216b0e7cb5617277711a79aa124dad
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2020-09-25 09:53:36 +0200

    #16 eslint, always define return type

--- example-code.ts
+++ example-code.ts
@@ -2,6 +2,6 @@ export function helloWorld(): void {
   console.log('Hello world this is a test');
 }
 
-export function add(a: number, b: number) {
+export function add(a: number, b: number): number {
   return a + b;
 }
